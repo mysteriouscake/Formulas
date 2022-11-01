9afe56ed2688c75d12aec4f5da7c89aba1103d74
Theodore <theo12398@outlook.com>
2022-11-01 16:12:59 -0500

    Initial Commit

---
+++ quadratic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  quadratic
+//
+//  Created by Theodore on 11/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quadratic/quadraticApp.swift
@@ -0,0 +1,17 @@
+//
+//  quadraticApp.swift
+//  quadratic
+//
+//  Created by Theodore on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct quadraticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
