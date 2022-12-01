7bc53a5035b61502fc9f6a2b311b7abde3c54057
Theodore <theo12398@outlook.com>
2022-11-30 21:18:15 -0600

    whew, close call (-/+ change integration)

--- quadratic/slopeview.swift
+++ quadratic/slopeview.swift
@@ -7,52 +7,94 @@
 
 import SwiftUI
 //swiftui
+import AudioToolbox
+//haptiiiiiiiiics
 struct slopeview: View {
     //vars
     @State var xOne: Float = 0.0
     @State var xTwo: Float = 0.0
     @State var yOne: Float = 0.0
     @State var yTwo: Float = 0.0
-    var body: some View {
-        VStack{
-            Text("Y-Intercept Form and Point Slope Form")
+    //this environment here just tells the app when it's dark mode or light mode to change assets accordingly
+    @Environment(\.colorScheme) var colorScheme
+    //switches -/+
+    func switchSigns(input: Float) -> Float{
+        let output = input * -1.0
+        AudioServicesPlaySystemSound(1520)
+        return output
+    }
+        var body: some View {
+            VStack{
+                Text("Y-Intercept Form and Point Slope Form")
                     .font(.title)
                     .multilineTextAlignment(.trailing)
                     .padding()
-            VStack{
-                Text("Y-Intercept Form")
-                Text("y = \((yTwo-yOne)/(xTwo-xOne))x + \(yOne-(((yTwo-yOne)/(xTwo-xOne))*xOne))")
-                }
-            VStack{
-                Text("Point Slope Form")
-                Text("y - \(yOne) = \((yTwo-yOne)/(xTwo-xOne))(x - \(xOne))")
-                }
-            HStack{
                 VStack{
-                    Text("X One = \(xOne)")
-                    Text("Y One = \(yOne)")
-                }.padding()
-                VStack{
-                    Text("X Two = \(xTwo)")
-                    Text("Y Two = \(yTwo)")
-                }.padding()
-            }
-            HStack{
-                VStack{
-                    TextField("X One", value: $xOne, formatter: NumberFormatter())
-                    TextField("Y One", value: $yOne, formatter: NumberFormatter())
+                    Text("Y-Intercept Form")
+                    Text("y = \((yTwo-yOne)/(xTwo-xOne))x + \(yOne-(((yTwo-yOne)/(xTwo-xOne))*xOne))")
                 }
                 VStack{
-                    TextField("X Two", value: $xTwo, formatter: NumberFormatter())
-                    TextField("Y Two", value: $yTwo, formatter: NumberFormatter())
+                    Text("Point Slope Form")
+                    Text("y - \(yOne) = \((yTwo-yOne)/(xTwo-xOne))(x - \(xOne))")
+                }
+                HStack{
+                    VStack{
+                        Text("X One = \(xOne)")
+                        Text("Y One = \(yOne)")
+                    }.padding()
+                    VStack{
+                        Text("X Two = \(xTwo)")
+                        Text("Y Two = \(yTwo)")
+                    }.padding()
+                }
+                HStack{
+                    VStack{
+                        HStack{
+                            TextField("X One", value: $xOne, formatter: NumberFormatter())
+                            Button{
+                                xOne = switchSigns(input: xOne)
+                            } label: {
+                                Image(colorScheme == .dark ? "plusminus" : "plusminuslight")
+                                    .padding()
+                            }
+                        }
+                        HStack{
+                            TextField("Y One", value: $yOne, formatter: NumberFormatter())
+                            Button{
+                                yOne = switchSigns(input: yOne)
+                            } label: {
+                                Image(colorScheme == .dark ? "plusminus" : "plusminuslight")
+                                    .padding()
+                            }
+                        }
+                    }
+                    VStack{
+                        HStack{
+                            TextField("X Two", value: $xTwo, formatter: NumberFormatter())
+                            Button{
+                                xTwo = switchSigns(input: xTwo)
+                            } label: {
+                                Image(colorScheme == .dark ? "plusminus" : "plusminuslight")
+                                    .padding()
+                            }
+                        }
+                        HStack{
+                            TextField("Y Two", value: $yTwo, formatter: NumberFormatter())
+                            Button{
+                                yTwo = switchSigns(input: yTwo)
+                            } label: {
+                                Image(colorScheme == .dark ? "plusminus" : "plusminuslight")
+                                    .padding()
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-}
-
-struct slopeview_Previews: PreviewProvider {
-    static var previews: some View {
-        slopeview()
+    
+    struct slopeview_Previews: PreviewProvider {
+        static var previews: some View {
+            slopeview()
+        }
     }
-}
