e7ba4600aeda08bb4d71f5392f8843cbb30dc151
Theodore Vu <114425635+mysteriouscake@users.noreply.github.com>
2023-01-17 12:24:42 -0600

    add some vars and ui stuffs

--- quadratic/rectangleview.swift
+++ quadratic/rectangleview.swift
@@ -7,11 +7,25 @@
 
 import SwiftUI
 
+import AudioToolbox
+
 struct rectangleview: View {
     @State var length: Float = 0.0
     @State var width: Float = 0.0
+     //this environment here just tells the app when it's dark mode or light mode to change assets accordingly
+    @Environment(\.colorScheme) var colorScheme
+    //switches -/+
+    func switchSigns(input: Float) -> Float{
+        let output = input * -1.0
+        AudioServicesPlaySystemSound(1520)
+        return output
+    }
     var body: some View {
-        Text("Area for Quadrilaterals")
+        VStack{
+            Text("Area for Quadrilaterals")
+                .font(.title)
+                .padding()
+        }
     }
 }
 
