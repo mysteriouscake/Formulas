e0ed8c60a194eefa1726b41b04b693993c4b25fd
Theodore <theo12398@outlook.com>
2023-02-09 19:03:12 -0600

    started some more area stuff

---
+++ quadratic/area/circleview.swift
@@ -0,0 +1,20 @@
+//
+//  circleview.swift
+//  quadratic
+//
+//  Created by Theodore on 2/9/23.
+//
+
+import SwiftUI
+
+struct circleview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct circleview_Previews: PreviewProvider {
+    static var previews: some View {
+        circleview()
+    }
+}
---
+++ quadratic/area/trapezoidview.swift
@@ -0,0 +1,20 @@
+//
+//  trapezoidview.swift
+//  quadratic
+//
+//  Created by Theodore on 2/9/23.
+//
+
+import SwiftUI
+
+struct trapezoidview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct trapezoidview_Previews: PreviewProvider {
+    static var previews: some View {
+        trapezoidview()
+    }
+}
