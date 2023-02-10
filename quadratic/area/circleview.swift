0f47316399a8fbcff55f0e1cda1817bd1ae1c8ee
Theodore Vu <114425635+mysteriouscake@users.noreply.github.com>
2023-02-10 12:07:36 -0600

    putting things together, setting var

--- quadratic/area/circleview.swift
+++ quadratic/area/circleview.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct circleview: View {
+    @State var radius: Float = 0.0
+     //this environment here just tells the app when it's dark mode or light mode to change assets accordingly
+    @Environment(\.colorScheme) var colorScheme
+    //switches -/+
+    func switchSigns(input: Float) -> Float{
+        let output = input * -1.0
+        AudioServicesPlaySystemSound(1520)
+        return output
+    }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
--- quadratic/area/triangleview.swift
+++ quadratic/area/triangleview.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct triangleview: View {
+    @State var base: Float = 0.0
+    @State var height: Float = 0.0
+     //this environment here just tells the app when it's dark mode or light mode to change assets accordingly
+    @Environment(\.colorScheme) var colorScheme
+    //switches -/+
+    func switchSigns(input: Float) -> Float{
+        let output = input * -1.0
+        AudioServicesPlaySystemSound(1520)
+        return output
+    }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
