6badbf0515b0fe9c40ccb56245dfaf75ec8830af
Theodore <theo12398@outlook.com>
2023-06-26 14:14:15 -0500

    started work w/ rectangles

--- quadratic/area/triangleview.swift
+++ quadratic/area/triangleview.swift
@@ -12,14 +12,6 @@ import AudioToolbox
 struct triangleview: View {
     @State var base: Float = 0.0
     @State var height: Float = 0.0
-     //this environment here just tells the app when it's dark mode or light mode to change assets accordingly
-    @Environment(\.colorScheme) var colorScheme
-    //switches -/+
-    func switchSigns(input: Float) -> Float{
-        let output = input * -1.0
-        AudioServicesPlaySystemSound(1520)
-        return output
-    }
     func formatter(x: NumberFormatter) -> Formatter{
         x.usesSignificantDigits = true
         return x
--- quadratic/volview.swift
+++ quadratic/volview.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct volview: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            List{
+                NavigationLink("Rectangular Prism", destination: rectprism())
+            }
+        }
     }
 }
 
