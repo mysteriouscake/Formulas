4ad2788aa227489f9c7a8f8e20abca73e7493b7d
Theodore <theo12398@outlook.com>
2023-06-26 14:50:47 -0500

    things look better now

--- quadratic/ContentView.swift
+++ quadratic/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Text("Formulas")
+            .padding()
+            .font(.largeTitle)
+            .border(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/3/*@END_MENU_TOKEN@*/)
         //now this is navigation
         NavigationStack{
             List{
                 NavigationLink("Quadratic", destination: quadview())
                 NavigationLink("Slope", destination: slopeview())
                 NavigationLink("Areas", destination: areaview())
-                //NavigationLink("Volumes", destination: volview())
-                /*
-                TODO FOR 4.0:
-                Volumes
-                */
+                NavigationLink("Volumes", destination: volview())
             }
         }
     }
